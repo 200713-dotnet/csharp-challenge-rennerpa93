a2582db6fd04fd8c95d6cba7a47a5e7ce0a47e73
patrickrenner <rennerpa93@gmail.com>
2020-07-20 17:12:50 -0400

    added test

--- Palindrome.Testing/UnitTest1.cs
+++ Palindrome.Testing/UnitTest1.cs
@@ -3,12 +3,16 @@ using Xunit;
 
 namespace Palindrome.Testing
 {
-    public class UnitTest1
+  public class UnitTest1
+  {
+    [Fact]
+    public void IsPalindromeTest()
     {
-        [Fact]
-        public void IsPalindromeTest()
-        {
-          
-        }
+      var sut = new PalindromeChecker();
+
+
+      Assert.IsType<PalindromeChecker>(sut);
+      Assert.NotNull(sut);
     }
+  }
 }
